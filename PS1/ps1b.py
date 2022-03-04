c73a63c60cd21980a8c5002277c2ef665cee14a2
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2022-03-04 21:38:56 +0200

    Update ps1b.py

--- PS1/ps1b.py
+++ PS1/ps1b.py
@@ -13,7 +13,7 @@
 def dp_make_weight(egg_weights, target_weight, memo = {}):
     """
     Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
-    an infinite supply of eggs of each weight, and there is always an egg of value 1.
+    an infinite supply of eggs of each weight, and there is always a egg of value 1.
     
     Parameters:
     egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
@@ -22,30 +22,36 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     
     Returns: int, smallest number of eggs needed to make target weight
     """
-    if egg_weights==() or target_weight ==0:
+    if (target_weight, egg_weights) in memo:
+      result = memo[(target_weight, egg_weights)]
+    if egg_weights==() or target_weight == 0:
       result =(0,())
     elif egg_weights[0] > target_weight:
-      result =dp_make_weight(egg_weights[1:], target_weight)
-      
+      result =dp_make_weight(egg_weights[1:], target_weight, memo)
     else:
       consider=egg_weights[0]
-      withVal, withToTake =dp_make_weight(egg_weights, (target_weight-consider))
+      withVal, withToTake =dp_make_weight(egg_weights, (target_weight-consider), memo)
       withVal += 1
       result = (withVal,withToTake+(consider,))
-    return result
-
-        
-        
       
-    
+    return (result)
+  
 
-# EXAMPLE TESTING CODE, feel free to add more if you'd like
+    
+# TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
     egg_weights = (1, 5, 10, 25)
-    egg_weights=egg_weights[-1::-1]
+    egg_weights =egg_weights[::-1]
     n = 99
+    number_eggs , list_eggs = dp_make_weight(egg_weights, n)
+    eggs_dict ={}
+    for egg in list_eggs:
+      if egg not in eggs_dict:
+        eggs_dict[egg] = 1
+      else:
+        eggs_dict[egg] +=1
     print("Egg weights = (1, 5, 10, 25)")
     print("n = 99")
     print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
-    print("Actual output:", dp_make_weight(egg_weights, n))
+    print("Actual output:", number_eggs, eggs_dict)
     print()
